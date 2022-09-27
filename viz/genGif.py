9153e63933fb1a4020f672a5ce3a63ce8cf0a93d
ewyser <manuwyser@gmail.com>
2022-09-27 16:58:34 +0200

    minor mods in python scripts

--- viz/genGif.py
+++ viz/genGif.py
@@ -1,5 +1,6 @@
+# python genGif.py <PathNNameOfTheVidToBeGiffed.VideoFormat>
+# python genGif.py /../vidTest.mp4
 import sys
 from moviepy.editor import VideoFileClip
 VideoFileClip(sys.argv[1]).write_gif("vidToGif.gif")
 # https://www.freecodecamp.org/news/how-to-convert-video-files-to-gif-in-python/
-# python genGif.py <PathNNameOfTheVidToBeGiffed>
